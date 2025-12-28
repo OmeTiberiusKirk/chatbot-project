fe1f69bc2bfeb1abb25f922353859f4ad85434d2
nukool <nukool@40.co.th>
2025-12-29 02:01:07 +0700

    ~

--- backend/app/models.py
+++ backend/app/models.py
@@ -2,6 +2,21 @@ import uuid
 
 from pydantic import EmailStr
 from sqlmodel import Field, Relationship, SQLModel
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import Mapped
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Documennt(Base):
+    __tablename__ = "documents"
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+
+Base.metadata.create_all(engine)
 
 
 # Shared properties
